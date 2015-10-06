9e87bde3941ccb531d52c9093491fbf6551774f2
Thierry Delbart <thierry@thde.pro>
2015-10-06 14:12:28 -0300

    Adding method resetear

--- server/startup.js
+++ server/startup.js
@@ -1,57 +1,64 @@
+Meteor.methods({
+   resetear: function() {
+
+		if (process.env.URBS_KEY) {
+			HTTP.get("http://transporteservico.urbs.curitiba.pr.gov.br/getPois.php?c=" + process.env.URBS_KEY,
+			  {},
+			  function (error, result) {
+			    if (!error) {
+			    	if (result.content) {
+			    		Pontos.remove({});
+			    	
+				    	pois = JSON.parse(result.content);
+				    	var vendas = ["BANCA DE REVISTA - CARTÃO TRANSPORTE","TERMINAL DE TRANSPORTE - CARTÃO TRANSPORTE","ESTAÇÃO TUBO - CARTÃO TRANSPORTE"];
+				    	var postos = ["RUA DA CIDADANIA - CARTÃO TRANSPORTE","POSTOS DE ATENDIMENTO URBS - CARTÃO TRANSPORTE"];
+				    	
+				    	pois.forEach(function(poi) {
+				    		
+				    		var type = false;
+				    		if (vendas.indexOf(poi.POI_CATEGORY_NAME) > -1) type = 'venda';
+				    		if (postos.indexOf(poi.POI_CATEGORY_NAME) > -1) type = 'posto';
+				    		
+				    		if (type) {
+				    			str = poi.POI_NAME.split(' - ');
+				    			if (str.length == 1) str = poi.POI_NAME.split('- ');
+				    			
+				    			name = str.shift().trim();
+				    			if (type == 'posto') str.push(poi.POI_DESC.trim());
+				    			desc =  str.join('<br />');
+				    			
+				    			doc = {
+							    		name: name,
+							    		type: type, 
+							    		lon: poi.POI_LON.replace(',','.'), 
+							    		lat: poi.POI_LAT.replace(',','.'), 
+							    		address: desc 
+							    };
+							    Pontos.insert(doc);
+							    console.log('Inserting Ponto: '+doc.name);
+				    		}
+			
+				    	});
+				    } else {
+				    	console.log("Error: no content found in URBS webservice");
+				    }
+			    	
+			    } else {
+			    	console.log("Error: URBS webservice unavailable");
+			    }
+			  }
+			);
+		} else {
+			console.log('Error: URBS_KEY not defined');
+		}
+
+   }
+});
+
 Meteor.startup(function () {
     // code to run on server at startup
     
-	if (process.env.URBS_KEY) {
-		HTTP.get("http://transporteservico.urbs.curitiba.pr.gov.br/getPois.php?c=" + process.env.URBS_KEY,
-		  {},
-		  function (error, result) {
-		    if (!error) {
-		    	if (result.content) {
-		    		Pontos.remove({});
-		    	
-			    	pois = JSON.parse(result.content);
-			    	var vendas = ["BANCA DE REVISTA - CARTÃO TRANSPORTE","TERMINAL DE TRANSPORTE - CARTÃO TRANSPORTE","ESTAÇÃO TUBO - CARTÃO TRANSPORTE"];
-			    	var postos = ["RUA DA CIDADANIA - CARTÃO TRANSPORTE","POSTOS DE ATENDIMENTO URBS - CARTÃO TRANSPORTE"];
-			    	
-			    	pois.forEach(function(poi) {
-			    		
-			    		var type = false;
-			    		if (vendas.indexOf(poi.POI_CATEGORY_NAME) > -1) type = 'venda';
-			    		if (postos.indexOf(poi.POI_CATEGORY_NAME) > -1) type = 'posto';
-			    		
-			    		if (type) {
-			    			str = poi.POI_NAME.split(' - ');
-			    			if (str.length == 1) str = poi.POI_NAME.split('- ');
-			    			
-			    			name = str.shift().trim();
-			    			if (type == 'posto') str.push(poi.POI_DESC.trim());
-			    			desc =  str.join('<br />');
-			    			
-			    			doc = {
-						    		name: name,
-						    		type: type, 
-						    		lon: poi.POI_LON.replace(',','.'), 
-						    		lat: poi.POI_LAT.replace(',','.'), 
-						    		address: desc 
-						    };
-						    Pontos.insert(doc);
-						    console.log('Inserting Ponto: '+doc.name);
-			    		}
-		
-			    	});
-			    } else {
-			    	console.log("Error: no content found in URBS webservice");
-			    }
-		    	
-		    } else {
-		    	console.log("Error: URBS webservice unavailable");
-		    }
-		  }
-		);
-	} else {
-		console.log('Error: URBS_KEY not defined');
-	}
-		
+		Meteor.call("resetear");
 
 });
 
