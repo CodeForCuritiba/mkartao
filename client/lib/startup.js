4a1bf104b9ffd235f7c7c129dce787389c05c108
Thierry Delbart <thierry@thde.pro>
2019-03-11 09:26:08 -0300

    Removing console.log

--- client/lib/startup.js
+++ client/lib/startup.js
@@ -1,6 +1,5 @@
 accountsUIBootstrap3.setLanguage('pt-BR');
 
 Meteor.startup(function() {
-  console.log(Meteor.settings);
   GoogleMaps.load({ v: '3', key: Meteor.settings.public.gmapApiKey });
 });
